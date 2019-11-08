801d19ca792fd367fba3f5e6080c66e53df0ea90
erlendnils1 <erlendnils1@gmail.com>
2019-11-08 08:04:49 +0100

    Spotless for code style

--- interface-protoc-plugin-test/src/test/java/no/entur/abt/protoc/test/InterfaceProtoPluginTest.java
+++ interface-protoc-plugin-test/src/test/java/no/entur/abt/protoc/test/InterfaceProtoPluginTest.java
@@ -9,22 +9,20 @@ import no.entur.abt.proto.plugin.test.TopLevelIBuilder;
 
 public class InterfaceProtoPluginTest {
 
-    @Test
-    public void testInterfaces() {
-        BottomLevel.Builder builder = BottomLevel.newBuilder();
-        populateTopLevelFields(builder);
-        BottomLevel bottomLevel = builder.build();
+	@Test
+	public void testInterfaces() {
+		BottomLevel.Builder builder = BottomLevel.newBuilder();
+		populateTopLevelFields(builder);
+		BottomLevel bottomLevel = builder.build();
 
+		TopLevelI topLevelI = bottomLevel;
 
-        TopLevelI topLevelI = bottomLevel;
+		Assertions.assertEquals(bottomLevel.getBoolVal(), topLevelI.getBoolVal());
+		Assertions.assertEquals(bottomLevel.getStringVal(), topLevelI.getStringVal());
+		Assertions.assertEquals(bottomLevel.getInt64Val(), topLevelI.getInt64Val());
+	}
 
-        Assertions.assertEquals(bottomLevel.getBoolVal(), topLevelI.getBoolVal());
-        Assertions.assertEquals(bottomLevel.getStringVal(), topLevelI.getStringVal());
-        Assertions.assertEquals(bottomLevel.getInt64Val(), topLevelI.getInt64Val());
-    }
-
-
-    private void populateTopLevelFields(TopLevelIBuilder builder) {
-        builder.setBoolVal(true).setInt64Val(345).setStringVal("testString");
-    }
+	private void populateTopLevelFields(TopLevelIBuilder builder) {
+		builder.setBoolVal(true).setInt64Val(345).setStringVal("testString");
+	}
 }
